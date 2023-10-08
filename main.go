3590035f202a9d0ab4d25dfec04fbc87d7b3b1f5
Pedro Gardim <me@pedrogardim.com>
2023-10-08 08:46:09 +0200

    temp: testing .env file

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	mongoUri := os.Getenv("MONGO_URI")
+	fmt.Println(mongoUri)
+}
